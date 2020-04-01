215d61973e8635da3fd0c4e8216a1054d71a25c4
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-04-01 17:46:57 -0400

    added bank class and some tests

--- WebMVC/src/test/java/com/zipcode/moneymanager/WebMVC/AccountTest.java
+++ WebMVC/src/test/java/com/zipcode/moneymanager/WebMVC/AccountTest.java
@@ -1,21 +1,39 @@
 package com.zipcode.moneymanager.WebMVC;
 
 
-import org.junit.Test;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
 
 public class AccountTest {
 
     @Test
     public void getAccountData() {
-        //AccountData expected = new AccountData(1000, "Basic", "basic@gmail.com", 500, "basic");
-       // Account acc = new BasicAccount(expected);
-        //Assert.assertEquals(expected, acc.getAccountData());
-
+        AccountData expected = new AccountData("Khalil", "abc@gmail.com", 1000,"basic");
+        Account acc = new BasicAccount(expected);
+        Assert.assertEquals(expected, acc.getAccountData());
     }
 
     @Test
     public void deposit() {
+        AccountData accountData = new AccountData("Khalil", "abc@gmail.com", 1000,"basic");
+        Account basicAccount = new BasicAccount(accountData);
+        basicAccount.deposit(500);
+        float expected = 1500;
+        Assert.assertEquals(expected, basicAccount.getBalance(), .01d);
+    }
+
+    @Test
+    public void accountID(){
+        AccountData accountData = new AccountData("Khalil", "abc@gmail.com", 1000,"basic");
+        AccountData accountData2 = new AccountData("Aarti", "abc1@gmail.com", 1000,"basic");
+        AccountData accountData3 = new AccountData("Sarah", "abc@gmail.com", 1000,"basic");
+        Account basicAccount = new BasicAccount(accountData);
+        Account basicAccount2 = new BasicAccount(accountData2);
+        Account basicAccount3 = new BasicAccount(accountData3);
+        int expected = 3;
+        int actual = basicAccount3.getAccountData().getId();
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
