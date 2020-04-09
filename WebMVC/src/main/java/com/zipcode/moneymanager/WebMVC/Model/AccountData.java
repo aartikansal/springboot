66da4542860f7af40413f0d140b59571e6bdf6cc
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-04-09 12:06:16 -0400

    added account data model

--- WebMVC/src/main/java/com/zipcode/moneymanager/WebMVC/Model/AccountData.java
+++ WebMVC/src/main/java/com/zipcode/moneymanager/WebMVC/Model/AccountData.java
@@ -1,29 +1,45 @@
-package com.zipcode.moneymanager.WebMVC;
+package com.zipcode.moneymanager.WebMVC.Model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CustomerData")
 
 public class AccountData {
     // ID, CustomerName, Email, AccountNumber, AccountType
-    private static int count = 0;
-    private final int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "email")
     private  String email;
+    @Column(name = "AccountBalance")
     private float balance;
+    @Column(name = "AccountID")
     private int accountNumber;
+    @Column(name = "AccountType")
     private String typeAccount;
 
-    AccountData(int id,String name, String email, int accountNumber, String typeAccount) {
+    public AccountData(int id,String name, String email, int accountNumber, String typeAccount) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.accountNumber = accountNumber;
         this.typeAccount = typeAccount;
     }
-    AccountData(Integer id,String name, String email, float balance, String typeAccount) {
+    public AccountData(Integer id,String name, String email, float balance, String typeAccount) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.balance = balance;
         this.typeAccount = typeAccount;
     }
+
+    public AccountData(){
+
+    }
+
 
     public int getId() {
         return id;
